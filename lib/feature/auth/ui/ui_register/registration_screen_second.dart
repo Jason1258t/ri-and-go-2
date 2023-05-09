f14ed3cb13b804f6923c59350b9287ca242325c7
Jason1258t <ivandomogatskij@gmail.com>
2023-05-09 12:05:36 +0300

    даже рабочая регистрация

--- lib/feature/auth/ui/ui_register/registration_screen_second.dart
+++ lib/feature/auth/ui/ui_register/registration_screen_second.dart
@@ -11,6 +11,8 @@ import 'package:riandgo2/widgets/buttons/default_elevated_button.dart';
 import 'package:riandgo2/widgets/buttons/small_text_button.dart';
 import 'package:riandgo2/widgets/text_fields/base_text_form_field.dart';
 
+import '../../../../widgets/text_fields/base_password_field.dart';
+
 class SecondRegistrationScreen extends StatelessWidget {
   SecondRegistrationScreen({Key? key}) : super(key: key);
   final TextEditingController _nameController = TextEditingController();
