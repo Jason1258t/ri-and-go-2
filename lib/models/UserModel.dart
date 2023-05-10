be3e6423dac7d96f4ea39db23a93865e450a6eeb
Jason1258t <ivandomogatskij@gmail.com>
2023-05-10 20:23:26 +0300

    профиль на две трети шишки с блоком

---
+++ lib/models/UserModel.dart
@@ -0,0 +1,10 @@
+
+class User {
+  final String name;
+  final String url;
+  final String imageUrl = '';
+  final String email;
+  final String phone;
+
+  User({required this.name, required this.url, required this.email, required this.phone});
+}
