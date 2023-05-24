99653ec2446bc5f8a3229d58f4bb1cdf510a84ed
Jason1258t <ivandomogatskij@gmail.com>
2023-05-24 20:07:32 +0300

    анимации на регистрации и логине

--- lib/feature/carusel_slider/carusel.dart
+++ lib/feature/carusel_slider/carusel.dart
@@ -26,49 +26,52 @@ class _ainCarouselState extends State<MainCarousel> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height * 0.9,
-                child: PageView.builder(
-                    onPageChanged: (index) {
-                      setState(() {
-                        currentIndex = index;
-                      });
-                    },
-                    itemCount: elements.length,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: SizedBox(
-                          width: 100,
-                          height: 100,
-                          child: elements[index],
-                        ),
-                      );
-                    }),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              for (var i = 0; i < elements.length; i++)...[
-                buildIndicator(currentIndex == i),
-                SizedBox(width: MediaQuery.of(context).size.width * 0.03,),
-              ]
-            ],
-          )
-        ],
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * 0.9,
+                  child: PageView.builder(
+                      onPageChanged: (index) {
+                        setState(() {
+                          currentIndex = index;
+                        });
+                      },
+                      itemCount: elements.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SizedBox(
+                            width: 100,
+                            height: 100,
+                            child: elements[index],
+                          ),
+                        );
+                      }),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                for (var i = 0; i < elements.length; i++)...[
+                  buildIndicator(currentIndex == i),
+                  SizedBox(width: MediaQuery.of(context).size.width * 0.03,),
+                ]
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
